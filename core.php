bd98467b09adc86f096b0b24b3c855ff77281a3b
Masa <137173415+Zdvorik@users.noreply.github.com>
2025-03-20 10:14:33 +0100

    core.php

--- core.php
+++ core.php
@@ -80,32 +80,34 @@ function logout()
 function print_table()
 {
     global $database_table_users, $database_table_scores;
-    $sql = "SELECT s.user_id, s.score, s.time, u.username 
-            FROM `" . $database_table_scores . "` AS s 
-            INNER JOIN `" . $database_table_users . "` AS u ON s.user_id = u.user_id 
-            ORDER BY s.score DESC, s.time ASC LIMIT 10;";
-    
+    $sql = "SELECT s.user_id, s.score, s.time, u.username
+        FROM `" . $database_table_scores . "` AS s
+        INNER JOIN `" . $database_table_users . "` AS u ON s.user_id = u.user_id
+        WHERE s.score = (SELECT MAX(score) FROM `" . $database_table_scores . "` WHERE user_id = s.user_id)
+        ORDER BY s.score DESC, s.time ASC
+        LIMIT 10";
+
     global $database_connection;
     $result = $database_connection->query($sql);
 
     echo "<table class='table table-bordered table-dark table-hover'>";
     echo "<thead><tr><th scope='col'>Hráč</th><th scope='col'>Skóre</th><th scope='col'>Čas</th>";
-    
+
     if (isset($_SESSION['user'])) {
         echo "<th scope='col'>Smazat</th>";
     }
     echo "</tr></thead><tbody>";
-    
+
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             $time = $row['time'];
             $minutes = floor($time / 60);
             $seconds = $time % 60;
-            
+
             echo get_table_row($row['username'], $row['user_id'], $row['score'], $minutes . "m " . $seconds . "s");
         }
     }
-    
+
     echo "</tbody></table>";
 }
 
